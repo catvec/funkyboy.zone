e184bdd7c2cff587eb8a361eb9b5d4762410171b
Noah Huppert <contact@noahh.io>
2019-01-27 19:44:11 -0500

    Finished server init script

--- server-scripts/init.sh
+++ server-scripts/init.sh
@@ -25,6 +25,15 @@
 # Exit on any error
 set -e
 
+# Set root password
+echo "===== Setting root password"
+echo "(You will be prompted for a new password)"
+
+if ! passwd root; then
+	echo "Error: Failed to set root password" >&2
+	exit 1
+fi
+
 # Delete misc files in /root
 echo "===== Deleting misc files in /root"
 
@@ -41,3 +50,38 @@ if [ -d /root/* ]; then
 		exit 1
 	fi
 fi
+
+# Install Salt
+echo "===== Installing Salt"
+
+if ! which salt &> /dev/null; then
+	if ! xbps-install -Sy salt; then
+		echo "Error: Failed to install salt" >&2
+		exit 1
+	fi
+else
+	echo "Already installed"
+fi
+
+# Symlink salt files
+echo "===== Symlinking salt files"
+
+if [ ! -d /srv ]; then
+	if ! mkdir /srv; then
+		echo "Error: Failed to make /srv directory" >&2
+		exit 1
+	fi
+else
+	echo "/srv directory already exists"
+fi
+
+for symlink_dir in salt pillar; do
+	if [ ! -d "/srv/$symlink_dir" ]; then
+		if ! ln -s "/opt/funkyboy.zone/$symlink_dir" "/srv/$symlink_dir"; then
+			echo "Error: Failed to symlink /srv/$symlink_dir directory" >&2
+			exit 1
+		fi
+	else
+		echo "/srv/$symlink_dir directory already symlinked"
+	fi
+done
