f8c935302a564e5a073c24a90e662ca49497bd7b
Noah Huppert <contact@noahh.io>
2019-03-05 13:16:59 -0500

    infra: added goldblum.zone A records

--- client-scripts/infra.tf
+++ client-scripts/infra.tf
@@ -37,6 +37,10 @@ data "digitalocean_domain" "gondola-zone" {
 	name = "gondola.zone"
 }
 
+data "digitalocean_domain" "goldblum-zone" {
+	name = "goldblum.zone"
+}
+
 # {{{1 Create resources
 # {{{2 Droplet
 resource "digitalocean_droplet" "funkyboy-zone" {
@@ -130,6 +134,23 @@ resource "digitalocean_record" "gondola-zone-wildcard" {
 
 resource "digitalocean_record" "gondola-zone-apex" {
 	domain = "${data.digitalocean_domain.gondola-zone.name}"
+	type = "A"
+	ttl = "60" # seconds
+	name = "@"
+	value = "${digitalocean_droplet.funkyboy-zone.ipv4_address}"
+}
+
+# {{{3 goldblum.zone
+resource "digitalocean_record" "goldblum-zone-wildcard" {
+	domain = "${data.digitalocean_domain.goldblum-zone.name}"
+	type = "A"
+	ttl = "60" # seconds
+	name = "*"
+	value = "${digitalocean_droplet.funkyboy-zone.ipv4_address}"
+}
+
+resource "digitalocean_record" "goldblum-zone-apex" {
+	domain = "${data.digitalocean_domain.goldblum-zone.name}"
 	type = "A"
 	ttl = "60" # seconds
 	name = "@"
