fa85a8c86308bb9fab60347787e8a08033372904
Noah Huppert <contact@noahh.io>
2019-04-18 19:51:08 -0400

    terraform: funkyboy.zone: added keybase verification entry

--- terraform/0100_funkyboy_zone.tf
+++ terraform/0100_funkyboy_zone.tf
@@ -33,3 +33,11 @@ resource "digitalocean_record" "funkyboy-zone-dkim" {
 	name = "mail._domainkey"
 	value = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdcDWpmBhFyplyLIqtosoThOcuKMst2U4BSKdkmP0/MauCShyTK4xnMEWjc07hMogN5n39j66DoBxccO2KLf0BYOCAnl7aaaorM7hRujvgkg7gYwbYG2tm9TMQRUTnbVkfSCKN2sz6oftpQXYzZxU7rGwOtBqxK4SyMFz0V0rNSwIDAQAB"
 }
+
+resource "digitalocean_record" "funkyboy-zone-keybase" {
+	 domain = "${data.digitalocean_domain.funkyboy-zone.name}"
+	 type = "TXT"
+	 ttl = "60" # seconds
+	 name = "@"
+	 value = "keybase-site-verification=29OQirPLhHqrbRfkhsdWl45XyYDZ537bFU2sh1zsW-A"
+}
