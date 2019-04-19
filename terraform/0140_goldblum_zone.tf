336aeaf074d5d95ebf54cd3162a8f526b0391ecd
Noah Huppert <contact@noahh.io>
2019-04-18 20:10:47 -0400

    terraform: goldblum.zone: added keybase entry

--- terraform/0140_goldblum_zone.tf
+++ terraform/0140_goldblum_zone.tf
@@ -17,3 +17,11 @@ resource "digitalocean_record" "goldblum-zone-apex" {
 	name = "@"
 	value = "${digitalocean_droplet.funkyboy-zone.ipv4_address}"
 }
+
+resource "digitalocean_record" "goldblum-zone-keybase" {
+	domain = "${data.digitalocean_domain.goldblum-zone.name}"
+	type = "TXT"
+	ttl = "60" # seconds
+	name = "@"
+	value = "keybase-site-verification=WZW-zpLmYG-6wcbQolAisRi5lrynVWsT2TRDKUv4APM"
+}
