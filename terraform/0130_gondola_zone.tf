e4ef8e3863c7ebd46ec9befed5b4f892ce5eff59
Noah Huppert <contact@noahh.io>
2019-04-18 20:20:58 -0400

    terraform: gondola.zone: added keybase DNS entry

--- terraform/0130_gondola_zone.tf
+++ terraform/0130_gondola_zone.tf
@@ -17,3 +17,11 @@ resource "digitalocean_record" "gondola-zone-apex" {
 	name = "@"
 	value = "${digitalocean_droplet.funkyboy-zone.ipv4_address}"
 }
+
+resource "digitalocean_record" "gondola-zone-keybase" {
+	domain = "${data.digitalocean_domain.gondola-zone.name}"
+	type = "TXT"
+	ttl = "60" # seconds
+	name = "@"
+	value = "keybase-site-verification=crd9ptToil_MjfIHdmjch3MP5NmhnKnNXulZ5sEpA30"
+}
