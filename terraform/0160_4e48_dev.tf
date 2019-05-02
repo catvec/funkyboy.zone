2ad98e25fc2742e6d76510422444deead33ce21e
Noah Huppert <contact@noahh.io>
2019-05-02 17:23:38 -0400

    terraform: moved noahh.io to aws, made cloudfront distribution work with 4e48.dev & noahh.io

--- terraform/0160_4e48_dev.tf
+++ terraform/0160_4e48_dev.tf
@@ -1,3 +1,35 @@
+# Zone
+variable "domain_4e48_dev_name" {
+  type = "string"
+  description = "Name of 4e48.dev domain"
+  default = "4e48.dev"
+}
+
 resource "aws_route53_zone" "4e48-dev" {
-  name = "4e48.dev"
+  name = "${var.domain_4e48_dev_name}"
+}
+
+# Records
+resource "aws_route53_record" "4e48-dev-personal-website-wildcard" {
+  zone_id = "${aws_route53_zone.4e48-dev.id}"
+  name = "*.${aws_route53_zone.4e48-dev.name}"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.personal-website.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.personal-website.hosted_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "4e48-dev-personal-website-apex" {
+  zone_id = "${aws_route53_zone.4e48-dev.id}"
+  name = "${aws_route53_zone.4e48-dev.name}"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.personal-website.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.personal-website.hosted_zone_id}"
+    evaluate_target_health = true
+  }
 }
--- terraform/README.md
+++ terraform/README.md
@@ -10,3 +10,4 @@ Files are organized by their prefixes:
 - `0100-0199`: Networking
 - `0200-0299`: Compute
 - `0300-0399`: Storage
+- `0400-0499`: Websites
