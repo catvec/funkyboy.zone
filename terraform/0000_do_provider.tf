5e92eca280732499567d19fc5b7f5fc03d8d5a6d
Noah Huppert <contact@noahh.io>
2019-05-02 02:56:23 -0400

    terraform: re-ordered order of items in do provider file

--- terraform/0000_do_provider.tf
+++ terraform/0000_do_provider.tf
@@ -1,10 +1,10 @@
-# Setup provider
-provider "digitalocean" {
-  token = "${var.do_token}"
-}
-
 # Variables
 variable "do_token" {
   type = "string"
   description = "Digital Ocean API token"
+}
+
+# Setup provider
+provider "digitalocean" {
+  token = "${var.do_token}"
 }
