686b46cdcf9b256d5d16320207a5726bbc7dd173
Noah Huppert <contact@noahh.io>
2019-05-02 05:30:50 -0400

    terraform: made web content bucket publically accessable

--- terraform/0300_web_content.tf
+++ terraform/0300_web_content.tf
@@ -1,4 +1,32 @@
+variable "web_content_bucket_name" {
+  type = "string"
+  description = "Name of web content bucket"
+  default = "noahh-web-content"
+}
+
 resource "aws_s3_bucket" "web-content" {
-  bucket = "noahh-web-content"
+  bucket = "${var.web_content_bucket_name}"
   acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "web-content-read" {
+  bucket = "${aws_s3_bucket.web-content.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "web-content-public-read",
+  "Statement": [
+        {
+            "Sid": "AllowPublicAcces",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": [
+                "arn:aws:s3:::${var.web_content_bucket_name}/*"
+            ]
+        }
+  ]
+}
+POLICY
 }
