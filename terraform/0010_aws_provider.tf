03ba1ddd5aceef439e2bc7227b90f4fd225d8c61
Noah Huppert <contact@noahh.io>
2019-05-02 02:56:48 -0400

    terraform: changed names of aws provider vars

--- terraform/0010_aws_provider.tf
+++ terraform/0010_aws_provider.tf
@@ -1,15 +1,15 @@
 # Variables
-variable "aws_access_key" {
+variable "aws_access_key_id" {
   type = "string"
-  description = "AWS API access key"
+  description = "AWS API access key ID"
 }
 
-variable "aws_secret_key" {
+variable "aws_secret_access_key" {
   type = "string"
   description = "AWS API secret access key"
 }
 
-variable "aws_region" {
+variable "aws_default_region" {
   type = "string"
   description = "AWS region"
   default = "us-east-1"
@@ -17,7 +17,7 @@ variable "aws_region" {
 
 # Setup provider
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region = "${var.aws_region}"
+  access_key = "${var.aws_access_key_id}"
+  secret_key = "${var.aws_secret_access_key}"
+  region = "${var.aws_default_region}"
 }
