c0a4ff3c4cc32378de250b1cf4dc54b298a5dc7c
Noah Huppert <contact@noahh.io>
2019-06-16 12:38:31 -0400

    terraform: pointed craft.4e48.dev to minecraft server

--- terraform/0160_4e48_dev.tf
+++ terraform/0160_4e48_dev.tf
@@ -5,14 +5,20 @@ variable "domain_4e48_dev_name" {
   default = "4e48.dev"
 }
 
-resource "aws_route53_zone" "4e48-dev" {
+variable "minecraft_server_ipv4" {
+  type = "string"
+  description = "IPv4 address of minecraft server"
+  default = "34.238.12.88"
+}
+
+resource "aws_route53_zone" "domain-4e48-dev" {
   name = "${var.domain_4e48_dev_name}"
 }
 
 # Records
-resource "aws_route53_record" "4e48-dev-personal-website-wildcard" {
-  zone_id = "${aws_route53_zone.4e48-dev.id}"
-  name = "*.${aws_route53_zone.4e48-dev.name}"
+resource "aws_route53_record" "record-4e48-dev-personal-website-wildcard" {
+  zone_id = "${aws_route53_zone.domain-4e48-dev.id}"
+  name = "*.${aws_route53_zone.domain-4e48-dev.name}"
   type = "A"
 
   alias {
@@ -22,9 +28,9 @@ resource "aws_route53_record" "4e48-dev-personal-website-wildcard" {
   }
 }
 
-resource "aws_route53_record" "4e48-dev-personal-website-apex" {
-  zone_id = "${aws_route53_zone.4e48-dev.id}"
-  name = "${aws_route53_zone.4e48-dev.name}"
+resource "aws_route53_record" "record-4e48-dev-personal-website-apex" {
+  zone_id = "${aws_route53_zone.domain-4e48-dev.id}"
+  name = "${aws_route53_zone.domain-4e48-dev.name}"
   type = "A"
 
   alias {
@@ -32,4 +38,13 @@ resource "aws_route53_record" "4e48-dev-personal-website-apex" {
     zone_id = "${aws_cloudfront_distribution.personal-website.hosted_zone_id}"
     evaluate_target_health = true
   }
+}
+
+resource "aws_route53_record" "record-4e48-dev-minecraft" {
+  zone_id = "${aws_route53_zone.domain-4e48-dev.id}"
+  name = "minecraft.${aws_route53_zone.domain-4e48-dev.name}"
+  type = "A"
+  records = [
+    "${var.minecraft_server_ipv4}"
+  ]
 }
