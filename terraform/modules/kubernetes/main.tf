e272b698b6cf62928a6d8c6f8b5928a825c224fa
Noah Huppert <contact@noahh.io>
2022-05-28 11:12:35 -0400

    terraform: fixed kubernetes iterator

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -6,9 +6,9 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
   dynamic "node_pool" {
     for_each = var.node_pools
     content {
-	 name = node_pool.name
-	 size = node_pool.size
-	 node_count = node_pool.node_count
+	 name = node_pool.value.name
+	 size = node_pool.value.size
+	 node_count = node_pool.value.node_count
     }
   }
 }
