4cd1133b2d530266f45f231ac993b59d94e98924
Noah Huppert <contact@noahh.io>
2022-06-08 22:42:46 -0400

    kubernetes: tweaked options in scripts

--- kubernetes/base/argocd/scripts/get-admin-password.sh
+++ kubernetes/base/argocd/scripts/get-admin-password.sh
@@ -2,7 +2,10 @@
 declare -r PROG_DIR=$(realpath $(dirname "$0"))
 
 # Exit codes
-declare -ri EXIT_CODE_KUBECTL_GET_SECRET=10
+declare -ri EXIT_CODE_UNKNOWN_OPT=10
+declare -r EXIT_MSG_UNKNOWN_OPT="Unknown option"
+
+declare -ri EXIT_CODE_KUBECTL_GET_SECRET=11
 declare -r EXIT_MSG_KUBECTL_GET_SECRET="Failed to get ArgoCD admin password from secret"
 
 # Common
@@ -10,15 +13,12 @@ source "$PROG_DIR/../../../../scripts/common.sh"
 
 declare -r HELP_SCRIPT="get-admin-password.sh"
 declare -r HELP_SCRIPT_BLURB="Retrieve the ArgoCD admin password"
-declare -ra HELP_OPTIONS=("h?" "p:META")
+declare -ra HELP_OPTIONS=("h?" "p?")
 declare -ra HELP_OPTION_BLURBS=("Show help text" "Only output password and no other logs")
 declare -r HELP_BEHAVIOR="Uses kubectl to retrieve the secret in which the ArgoCD operator stores the admin user's password"
-export HELP_SCRIPT
-export HELP_SCRIPT_BLURB
-export HELP_OPTIONS
-export HEPL_OPTION_BLURBS
 
 # Options
+declare opt_plain=""
 while getopts "hp" opt; do
     case "$opt" in
 	   h)
@@ -26,12 +26,21 @@ while getopts "hp" opt; do
 		  exit 0
 		  ;;
 	   p) opt_plain="y" ;;
+	   '?') die "$EXIT_CODE_UNKNOWN_OPT" "$EXIT_MSG_UNKNOWN_OPT" ;;
     esac
 done
 
 # Get admin password
-log "Retrieving ArgoCD admin password"
-declare -r admin_password=$(run_check "kubectl -n argocd get secret argocd-cluster -o jsonpath='{.data.admin\.password}' | base64 -d" "$EXIT_CODE_KUBECTL_GET_SECRET" "$EXIT_MSG_KUBECTL_GET_SECRET") || exit
+if [[ -z "$opt_plain" ]]; then
+    log "Retrieving ArgoCD admin password"
+fi
 
-log "ArgoCD admin password:"
-log "  '$admin_password'"
+declare  admin_password
+admin_password=$(run_check "kubectl -n argocd get secret argocd-cluster -o jsonpath='{.data.admin\.password}' | base64 -d" "$EXIT_CODE_KUBECTL_GET_SECRET" "$EXIT_MSG_KUBECTL_GET_SECRET") || exit
+
+if [[ -z "$opt_plain" ]]; then
+    log "ArgoCD admin password:"
+    log "  '$admin_password'"
+else
+    echo "$admin_password"
+fi
--- kubernetes/base/argocd/scripts/re-download-argocd-operator-subscription-manifest.sh
+++ kubernetes/base/argocd/scripts/re-download-argocd-operator-subscription-manifest.sh
@@ -1,7 +1,38 @@
 #!/usr/bin/env bash
 declare -r PROG_DIR=$(realpath $(dirname "$0"))
 
+# Exit codes
+declare -ri EXIT_CODE_UNKNOWN_OPT=10
+declare -r EXIT_MSG_UNKNOWN_OPT="Unknown option"
+
+declare -ri EXIT_CODE_DOWNLOAD=11
+declare -r EXIT_MSG_DOWNLOAD="Failed to download ArgoCD operator subscription manifest."
+
+# Common
+source "$PROG_DIR/../../../../scripts/common.sh"
+
+declare -r HELP_SCRIPT="re-download-argocd-operator-subscription-manifest.sh"
+declare -r HELP_SCRIPT_BLURB="Re-downloads the ArgoCD operator subscription"
+declare -ra HELP_OPTIONS=("h?")
+declare -ra HELP_OPTION_BLURBS=("Show help text")
+declare -r HELP_BEHAVIOR="Downloads the ArgoCD operator OLM subscription manifest from the OperatorHub website"
+
+# Constants
 declare -r MANIFEST_URL="https://operatorhub.io/install/argocd-operator.yaml"
 declare -r MANIFEST_OUT="$PROG_DIR/../base/argocd-operator-subscription.yaml"
 
-curl -L "$MANIFEST_URL" -o "$MANIFEST_OUT"
+# Options
+while getopts "h" opt; do
+    case "$opt" in
+	   h)
+		  generate_help_msg
+		  exit 0
+		  ;;
+	   '?') die "$EXIT_CODE_UNKNOWN_OPT" "$EXIT_MSG_UNKNOWN_OPT" ;;
+    esac
+done
+
+# Download
+log "Downloading ArgoCD operator OLM subscription"
+run_check "curl -L '$MANIFEST_URL' -o '$MANIFEST_OUT'" "$EXIT_CODE_DOWNLOAD" "$EXIT_MSG_DOWNLOAD"
+log "Success"
