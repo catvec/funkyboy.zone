af85b6b9e826a3792ca8bd6faf8005a75646e730
Noah Huppert <contact@noahh.io>
2023-06-09 22:32:39 -0400

    client-scripts: setup-k8s: improving bin calls
    Making clients w Python APIs to wrap all the Popen calls.

--- client-scripts/setup-k8s.py
+++ client-scripts/setup-k8s.py
@@ -3,8 +3,9 @@ import argparse
 import subprocess
 import logging
 import os
-from typing import Optional, Literal, Union
+from typing import Optional, Literal, Union, List, TypedDict
 import re
+import sys
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -21,6 +22,12 @@ class KustomizeBuildError(Exception):
     """
     def __init__(self, returncode: int, stdout: Optional[str], stderr: Optional[str]):
         super().__init__(f"Failed to build manifests with Kustomize (exit code: {returncode}), stdout={stdout}, stderr={stderr}")
+
+class KustomizeBinNotFound(Exception):
+    """ Indicates the kustomize client could not be found.
+    """
+    def __init__(self):
+        super().__init__("Failed to find kustomize binary")
 
 class KubeDryRunError(Exception):
     """ Indicates that running Kubectl in dry run mode failed.
@@ -54,6 +61,207 @@ def decode_bytes(value: bytes) -> Optional[str]:
         return None
 
     return value.decode("utf-8")
+
+class KubeDryRunRes(TypedDict):
+    """ Results of Kubectl dry run.
+    Fields:
+    - missing_namespaces: List of namespaces which are missing
+    - output: Text sent by dry run
+    """
+    missing_namespaces: Optional[List[str]]
+    output: str
+
+
+class KubeStderrMissingNamespaces(TypedDict):
+    """ Results of decoding the kubectl stderr to check for missing namespace errors.
+    Fields:
+    - missing_namespaces: The namespaces which were missing
+    - only_missing_namespaces_errors: If the only errors in the output were missing namespaces errors
+    """
+    missing_namespaces: Optional[List[str]]
+    only_missing_namespaces_errors: bool
+
+class KubeDiffRes(TypedDict):
+    """ Results of kubectl diff.
+    Arguments:
+    - missing_namespaces: The namespaces which were missing from the server
+    - diff: Diff output
+    """
+    missing_namespaces: Optional[List[str]]
+    diff: str
+
+class KubectlClient:
+    """ Client for kubectl tool.
+    Fields:
+    - kubeconfig_path: Location of Kubeconfig file
+    """
+    kubeconfig_path: str
+
+    def __init__(self, kubeconfig_path: str):
+        """ Initialize KubectlClient.
+        Arguments:
+        - kubeconfig_path: See KubectlClient.kubeconfig_path
+        """
+        self.kubeconfig_path = kubeconfig_path
+
+    def __check_stderr_for_missing_namespaces(self, stderr: str) -> KubeStderrMissingNamespaces:
+        """ Decode kubectl's stderr output to check for missing namespace errors.
+        Arguments:
+        - stderr: Text to check
+
+        Returns: Results of decoding
+        """
+        namespace_error_re = re.compile("^Error from server \(NotFound\): namespaces \"(.*)\" not found$")
+        missing_namespaces = set()
+        only_namespace_errors = True
+
+        for stderr_line in stderr.strip().split("\n"):
+            err_match = namespace_error_re.match(stderr_line.strip())
+            if err_match:
+                missing_namespaces.add(err_match.group(1))
+            else:
+                only_namespace_errors = False
+
+        return {
+            'missing_namespaces': missing_namespaces,
+            'only_missing_namespaces_errors': only_namespace_errors,
+        }
+
+    def dry_run(self, action: Union[Literal["apply"], Literal["delete"]], input_manifests: str) -> KubeDryRunRes:
+        """ Dry run applying manifests against the server.
+        Arguments:
+        - action: Whether the dry run should simulate an apply or delete
+        - input_manifests: Input YAML to dry run apply
+
+        Returns: Results of dry run
+
+        Raises:
+        - KubeDryRunError: If dry run failed
+        """
+        res = subprocess.Popen(
+            ["kubectl", action, "-f", "-", "--dry-run=server"],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=dict(os.environ, KUBECONFIG=self.kubeconfig_path),
+        )
+        out = res.communicate(input=input_manifests)
+
+        if res.wait() != 0:
+            # Ignore if the only errors are about namespaces not existing
+            stderr = decode_bytes(out[1])
+            
+            ns_errs = self.__check_stderr_for_missing_namespaces(stderr)
+
+            if not ns_errs['only_missing_namespaces_errors']:
+                raise KubeDryRunError(
+                    returncode=res.returncode,
+                    stdout=decode_bytes(out[0]),
+                    stderr=stderr,
+                )
+
+            return {
+                'missing_namespaces': ns_errs['missing_namespaces'],
+                'output': decode_bytes(out[0]),
+            }
+        
+        return {
+            'output': decode_bytes(out[0]),
+        }
+
+    def diff(self, input_manifests: str) -> KubeDiffRes:
+        """ Determine difference between resources on server and provided manifests.
+        Arguments:
+        - input_manifests: YAML of resources of which to diff
+
+        Returns: Diff results
+
+        Raises:
+        - KubeDiffError: If error occurs when diffing
+        """
+        res = subprocess.Popen(
+            ["kubectl", "diff", "-f", "-"],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=dict(os.environ, KUBECONFIG=self.kubeconfig_path),
+        )
+        out = res.communicate(input=input_manifests)
+        
+        if res.wait() > 1:
+            # Ignore missing namespace errors
+            stderr = decode_bytes(out[1])
+
+            ns_errs = self.__check_stderr_for_missing_namespaces(stderr)
+
+            if not ns_errs['only_missing_namespaces_errors']:
+                raise KubeDiffError(
+                    returncode=res.returncode,
+                    stdout=decode_bytes(out[0]),
+                    stderr=stderr,
+                )
+            
+        return {
+            'diff': decode_bytes(out[0]),
+        }
+
+
+class KustomizeClient:
+    """ Client which invokes Kustomize.
+    Arguments:
+    - __kustomize_cmd_name: Internal field used to memoize the found kustomize cmd name
+    """
+    __kustomize_cmd_name: Optional[List[str]]
+
+    def __init__(self):
+        self.__kustomize_cmd_name = None
+
+    def __build_cmd(self, args: List[str]) -> List[str]:
+        """ Constructs a command line string given the arguments.
+        Arguments:
+        - args: Kustomize arguments
+
+        Returns: Kustomize command line construction
+
+        Raises:
+        - KustomizeBinNotFound: If no Kustomize binary could not be found
+        """
+        if self.__kustomize_cmd_name is None:
+            if sys.executable("kustomize") is not None:
+                self.__kustomize_cmd_name = ["kustomize"]
+            elif sys.executable("kubectl") is not None:
+                self.__kustomize_cmd_name = ["kubectl"]
+            else:
+                raise KustomizeBinNotFound()
+
+        return self.__kustomize_cmd_name + args
+        
+
+    def build(self, dir: str) -> str:
+        """ Use Kustomize to build a Kustomize project.
+        Arguments:
+        - dir: Directory of Kustomize project
+
+        Returns: Kustomize YAML output
+
+        Raises:
+        - KustomizeBuildError: If build encounters an error
+        """        
+        res = subprocess.Popen(
+            self.__build_cmd(["build", dir]),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out = res.communicate()
+
+        if res.wait() != 0:
+            raise KustomizeBuildError(
+                returncode=res.returncode,
+                stdout=decode_bytes(out[0]),
+                stderr=decode_bytes(out[1]),
+            )
+        
+        return out[0]
 
 def main():
     """ Entrypoint.
@@ -123,23 +331,13 @@ def render_and_apply_or_delete(
     show_manifests: bool,
     verbose: bool,
 ):
+    kubectl = KubectlClient(kubeconfig_path=KUBECONFIG_PATH)
+    kustomize = KustomizeClient()
+    
     # Render Kubernetes manifests
     logging.info("Building manifests with Kustomize")
-
-    kustomize_res = subprocess.Popen(
-        ["kustomize", "build", target_dir],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    kustomize_out = kustomize_res.communicate()
-
-    if kustomize_res.wait() != 0:
-        raise KustomizeBuildError(
-            returncode=kustomize_res.returncode,
-            stdout=decode_bytes(kustomize_out[0]),
-            stderr=decode_bytes(kustomize_out[1]),
-        )
-    kustomize_build_str = kustomize_out[0]
+    
+    kustomize_build_str = kustomize.build(target_dir)
 
     logging.info("Successfully built manifests")
 
@@ -147,43 +345,14 @@ def render_and_apply_or_delete(
     if not no_validate:
         logging.info("Validating manifests")
 
-        kubectl_dry_run_res = subprocess.Popen(
-            ["kubectl", action, "-f", "-", "--dry-run=server"],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            env=dict(os.environ, KUBECONFIG=KUBECONFIG_PATH),
-        )
-        kubectl_dry_run_out = kubectl_dry_run_res.communicate(input=kustomize_build_str)
+        dry_run_res = kubectl.dry_run(kustomize_build_str)
         
-        if kubectl_dry_run_res.wait() != 0:
-            # Ignore if the only errors are about namespaces not existing
-            kubectl_dry_run_stderr = decode_bytes(kubectl_dry_run_out[1])
-            
-            namespace_error_re = re.compile("^Error from server \(NotFound\): error when creating \"STDIN\": namespaces \"(.*)\" not found$")
-            only_namespace_errors = True
-            missing_namespaces = set()
-
-            for stderr_line in kubectl_dry_run_stderr.strip().split("\n"):
-                err_match = namespace_error_re.match(stderr_line.strip())
-                if err_match:
-                    missing_namespaces.add(err_match.group(1))
-                else:
-                    only_namespace_errors = False
-
-            for ns in missing_namespaces:
+        if 'missing_namespaces' in dry_run_res:
+            for ns in dry_run_res['missing_namespaces']:
                 logging.warning("Must create namespace: %s", ns)
 
-            if only_namespace_errors:
-                logging.warning("Validation might not be accurate because resource(s) were specified in namespace(s) which do not exist")
-
-            if not only_namespace_errors:
-                raise KubeDryRunError(
-                    returncode=kubectl_dry_run_res.returncode,
-                    stdout=decode_bytes(kubectl_dry_run_out[0]),
-                    stderr=kubectl_dry_run_stderr,
-                )
-
+            logging.warning("Validation might not be accurate because resource(s) were specified in namespace(s) which do not exist")
+        
         logging.info("Validated manifests")
     else:
         logging.info("Not validating manifests")
@@ -192,45 +361,16 @@ def render_and_apply_or_delete(
     if not no_diff and action == "apply":
         logging.info("Preparing diff")
 
-        kubectl_diff_res = subprocess.Popen(
-            ["kubectl", "diff", "-f", "-"],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            env=dict(os.environ, KUBECONFIG=KUBECONFIG_PATH),
-        )
-        kubectl_diff_out = kubectl_diff_res.communicate(input=kustomize_build_str)
-        
-        if kubectl_diff_res.wait() > 1:
-            # Ignore missing namespace errors
-            kubectl_diff_stderr = decode_bytes(kubectl_diff_out[1])
+        diff_res = kubectl.diff()
 
-            namespace_error_re = re.compile("^Error from server \(NotFound\): namespaces \"(.*)\" not found$")
-            missing_namespaces = set()
-            only_namespace_errors = True
-
-            for stderr_line in kubectl_diff_stderr.strip().split("\n"):
-                err_match = namespace_error_re.match(stderr_line.strip())
-                if err_match:
-                    missing_namespaces.add(err_match.group(1))
-                else:
-                    only_namespace_errors = False
-
-            for ns in missing_namespaces:
+        if 'missing_namespaces' in diff_res:
+            for ns in diff_res['missing_namespaces']:
                 logging.warning("Must create namespace: %s", ns)
 
-            if only_namespace_errors:
-                logging.warning("Diff might not be accurate because resource(s) were specified in namespace(s) which do not exist")
-
-            if not only_namespace_errors:
-                raise KubeDiffError(
-                    returncode=kubectl_diff_res.returncode,
-                    stdout=decode_bytes(kubectl_diff_out[0]),
-                    stderr=kubectl_diff_stderr,
-                ) 
+            logging.warning("Diff might not be accurate because resource(s) were specified in namespace(s) which do not exist")
 
         logging.info("Proposed manifest changes")
-        logging.info(decode_bytes(kubectl_diff_out[0]))
+        logging.info(diff_res['diff'])
         
         logging.info("Confirm changes [y/N]")
         confirm_in = input().strip().lower()
