6e2a40fad7ec461700aae2a948693968da7ee38a
Noah Huppert <contact@noahh.io>
2023-06-10 01:07:30 -0400

    client-scripts: setup-k8s: disabled kustomize re-ordering

--- client-scripts/setup-k8s.py
+++ client-scripts/setup-k8s.py
@@ -288,7 +288,7 @@ class KustomizeClient:
         - KustomizeBuildError: If build encounters an error
         """        
         res = subprocess.Popen(
-            self.__build_cmd([dir]),
+            self.__build_cmd(["--reorder=none", dir]),
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
