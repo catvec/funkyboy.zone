62907d5591f47070201c520f0eddfcfc9389c203
Noah Huppert <contact@noahh.io>
2025-06-14 14:29:29 -0400

    k8s: funkyboy: updated w side images

--- kubernetes/base/funkyboy-zone/content/build-and-push.sh
+++ kubernetes/base/funkyboy-zone/content/build-and-push.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 readonly PROG_DIR=$(realpath $(dirname "$0"))
 readonly DOCKER=docker
-readonly IMAGE_TAG="2025-06-14-4"
+readonly IMAGE_TAG="2025-06-14-6"
 readonly IMAGE_NAME="isolation0230/funkyboy-zone"
 readonly IMAGE_FULLNAME="${IMAGE_NAME}:${IMAGE_TAG}"
 
